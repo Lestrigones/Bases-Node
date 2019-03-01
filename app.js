1a2b9a4d35bfcc806987d3108b5b750d72580afa
Lestrigones <40467401+Lestrigones@users.noreply.github.com>
2019-03-01 18:07:01 +0100

    pequeño tutorial js

---
+++ app.js
@@ -0,0 +1,35 @@
+const argv = require('./config/yargs').argv;
+const colors = require('colors/safe');
+
+const { crearArchivo, listarTabla } = require('./multiplicar/multiplicar');
+
+let comando = argv._[0];
+
+
+switch (comando) {
+
+    case 'listar':
+        listarTabla(argv.base, argv.limite);
+        break;
+
+    case 'crear':
+        crearArchivo(argv.base, argv.limite)
+            .then(archivo => console.log(`Archivo creado: `, colors.green(archivo)))
+            .catch(e => console.log(e));
+        break;
+
+    default:
+        console.log('Comando no reconocido');
+
+}
+
+//let base = 3;
+//console.log(multiplicar);
+
+//let argv2 = process.argv;
+//console.log('limite', argv.limite);
+//console.log(argv2);
+//let parametro = argv[2];
+//let base = parametro.split('=')[1]
+
+//console.log(parametro);
